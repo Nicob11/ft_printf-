0be7d7800a2d8ea0b1ee3d8cd31184964a492164
Nicob11 <nicobasma_@hotmail.com>
2024-01-10 16:03:56 +0100

    brach dev created

---
+++ libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: febasma <febasma@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 16:49:30 by febasma           #+#    #+#             */
+/*   Updated: 2023/11/08 16:58:07 by febasma          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <stdarg.h>
+//# include "./libft/libft.h"
+
+int        ft_printf(char const *, ...);
+
+#endif
